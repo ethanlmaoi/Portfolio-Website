9b5a16bc04e1c8688d22c9c1800e57d331ed29ce
Ethan Liao <30430093+ethanlmaoi@users.noreply.github.com>
2018-09-13 12:20:18 -0700

    added icon + icon animation
    updated background as well

--- assets/js/portf.js
+++ assets/js/portf.js
@@ -1,3 +1,20 @@
-$(document).ready(function () {
-    $('div.outlineMe').fadeIn(2200);
+$(document).ready(function() {
+    $(".outlineMe").fadeIn(2300);
+	$(".outlineMe").animate({
+        left: '0px', // how far you want to move to the left
+        opacity: '0.80', // how much you want to blend
+    });
+
 });
+
+$("#blueIconContainer").hover(function() {
+	$("#blueCircleIcon").animate({
+		height: "200px",
+		width: "200px",
+		
+	});}, function() {
+		$("#blueCircleIcon").animate({
+		height: "350px",
+		width: "350px",
+		});
+	});
